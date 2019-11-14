a7ef6fc3ff69f0bd0afa7fab8f5b31176c1f71b8
Igor AC <igorac1999@gmail.com>
2019-11-14 18:59:10 -0300

    Minor, update route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ Route::get('/login/logout', 'AuthController@logout')->name('logout');
  */
 Route::middleware('auth')->group( function(){
     Route::get('/tasks', 'TaskController@index')->name('home');
-    Route::get('/tasks/json', 'TaskController@tasksJson')->name('home.json');
     Route::post('/task/store', 'TaskController@store')->name('task.store');
     Route::delete('/task/delete/{id}', 'TaskController@delete')->name('task.delete')->where(['id' => '[0-9]+']);
     route::put('/task/update/{id}', 'TaskController@update')->name('task.update')->where(['id' => '[0-9]+']);
